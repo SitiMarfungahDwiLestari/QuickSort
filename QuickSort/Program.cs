6e4d3b8edc967602fbc0557b2d51d4544e975b21
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-10-21 16:57:39 +0700

    add array of integers

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -13,9 +13,6 @@ namespace QuickSort
         private int cmp_count = 0; //number of comparasion
         private int mov_count = 0; //number of data movement
 
-        //Number of elements in array
-        private int n;
-
 
 
 
