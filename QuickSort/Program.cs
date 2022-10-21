bf546c7cf2ba0f6d1e51d56dc3ba5372106fdc8f
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-10-21 17:43:20 +0700

    add if the greater element is on the left of the element

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -76,7 +76,14 @@ namespace QuickSort
                 }
                 cmp_count++;
 
-                
+                if ( i < j) //if the greater element is on the left of the element
+                {
+                    //swap the element at index i with the element at index j
+                    swap(i, j);
+                    mov_count++;
+                }
+            }
+            
         static void Main(string[] args)
         {
         }
