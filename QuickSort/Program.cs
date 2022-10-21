575cdf28e2c4df2c3e21934986ef659ce9fef1be
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-10-21 16:54:51 +0700

    Add project files.

---
+++ QuickSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        //array of integers to hold
+        private int[] arr = new int[20];
+        private int cmp_count = 0; //number of comparasion
+        private int mov_count = 0; //number of data movement
+
+        //Number of elements in array
+        private int n;
+
+
+
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
